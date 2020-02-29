5b9e4ae70e2fb9ee51df48f3309175d77cc7ee4e
OliverSjolind <oliver.sjolind@gmail.com>
2020-02-29 19:00:36 +0200

    made ob work

--- webshop_frontend/client/src/App/components/FilterBar.js
+++ webshop_frontend/client/src/App/components/FilterBar.js
@@ -1,21 +1,21 @@
-import React, { Component, forceUpdate } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 import M from 'materialize-css'
 import { handleQueryParams } from '../js/functions'
 
 class FilterBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            queryParamUrl: '',
-        }
+        this.orderChange = this.orderChange.bind(this)
+        this.componentDidUpdate = this.componentDidUpdate.bind(this)
     }
     componentDidUpdate(prevProps) {
         if (this.props.match.url !== prevProps.match.url) {
             // let dropDown = document.getElementById('order');
             // dropDown.selectedIndex = "0"
             // console.log(dropDown.selectedIndex)
-            this.forceUpdate()
+            // document.getElementById('order').selectedIndex = ""
+            // console.log(document.getElementById('order').selectedIndex);
+            document.getElementById('filterBar').reset()
         }
     }
 
@@ -25,6 +25,7 @@ class FilterBar extends Component {
     }
 
     orderChange = (e) => {
+        this.setState({ orderValue: e.target.value });
         if (e.target.value) {
             this.props.history.push(`${handleQueryParams(this.props.location.search, e.target.value)}`)
         } else {
@@ -35,24 +36,26 @@ class FilterBar extends Component {
     render() {
         return (
             <div className="row filter-bar">
-                <div className="input-field col s3">
-                    <input id="category-search" type="text" autoComplete="off" />
-                    <label htmlFor="category-search">Search</label>
-                </div>
-
-                <div className="col s5 mt5">
-                    <div id="price-slider"></div>
-                    <div className="price-range-filter"><input type="number" className="priceInput" id="minInput" />€ - <input type="number" className="priceInput" id="maxInput" />€ <a href="#" id="setprice">Set price range</a>
+                <form id="filterBar">
+                    <div className="input-field col s3">
+                        <input id="category-search" type="text" autoComplete="off" />
+                        <label htmlFor="category-search">Search</label>
                     </div>
-                </div>
 
-                <div className="input-field col s3 offset-s1">
-                    <select id="order" name="order" onChange={this.orderChange}>
-                        <option value="" defaultValue>Order by</option>
-                        <option id="lowHigh" className="orderOption" name="lowHigh" value="ob=priceAsc">price: low-high</option>
-                        <option className="orderOption" value="ob=priceDesc">price: high-low</option>
-                    </select>
-                </div>
+                    <div className="col s5 mt5">
+                        <div id="price-slider"></div>
+                        <div className="price-range-filter"><input type="number" className="priceInput" id="minInput" />€ - <input type="number" className="priceInput" id="maxInput" />€ <a href="#" id="setprice">Set price range</a>
+                        </div>
+                    </div>
+
+                    <div className="input-field col s3 offset-s1">
+                        <select id="order" name="order" onChange={this.orderChange}>
+                            <option value="">Order by</option>
+                            <option id="lowHigh" className="orderOption" name="lowHigh" value="ob=priceAsc">price: low-high</option>
+                            <option className="orderOption" value="ob=priceDesc">price: high-low</option>
+                        </select>
+                    </div>
+                </form>
             </div>
         )
     }
